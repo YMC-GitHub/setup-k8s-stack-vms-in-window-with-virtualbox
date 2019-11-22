971bc6f093c1a69e30f5bb3850bee808ba1ce41a
yemiancheng <ymc.github@gmail.com>
2019-11-22 16:50:06 +0800

    feat(core): initing basic core
      adds basic construtor
      adds action-function

---
+++ action-function.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+function clone() {
+    echo "clone vm"
+}
+function update() {
+    echo "update vm"
+}
+function start() {
+    echo "start vm"
+}
+function init_stack_master() {
+    echo "stack init k8s master"
+}
+function node_join_stack() {
+    echo "node join k8s stack"
+}
--- run-action.sh
+++ run-action.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 
-function clone() {
-    echo "clone vm"
-}
-function update() {
-    echo "update vm"
-}
-function start() {
-    echo "start vm"
-}
-function init_stack_master() {
-    echo "stack init k8s master"
-}
-function node_join_stack() {
-    echo "node join k8s stack"
-}
+source ./action-function.sh
 
 ACTION_LIST=$(cat action-list.txt | sed "s/^#.*//g" | sed "/^$/d")
 
